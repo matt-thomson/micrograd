0504f215e8f047cbf3ec13112217a3fbd04b209d
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2025-03-03 10:53:53 +0000

    Use value from example

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn calculate_neuron(h: f64) -> f64 {
     let x2: Value = (0.0).into();
     let w1: Value = (-3.0 + h).into();
     let w2: Value = (1.0).into();
-    let b: Value = (6.7).into();
+    let b: Value = (6.88137358701957432).into();
 
     let n = (x1 * w1) + (x2 * w2) + b;
     let o = n.tanh();
